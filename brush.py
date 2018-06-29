9433266f296595f24066185d06d9ccdec95c89b7
Petter Amland <petter@rain-games.com>
2018-06-29 17:22:12 +0200

    fixed color channels, brush opacity.

--- brush.py
+++ brush.py
@@ -1,14 +1,24 @@
 from ursina import *
-from PIL import Image, ImageChops
+from PIL import Image, ImageChops, ImageEnhance
 
 
 
 class Brush(Entity):
     def __init__(self):
         super().__init__()
-        self.org_brush = Image.open('textures/' + 'round_pixelated.png').transpose(Image.FLIP_TOP_BOTTOM)
+        self.org_brush = Image.open('textures/' + 'brush.png').transpose(Image.FLIP_TOP_BOTTOM)
+        # convert to brg color
+        channels = self.org_brush.split()
+        printvar(len(channels))
+        if len(channels) == 3:
+            self.org_brush = Image.merge("RGB", (channels[2], channels[1], channels[0]))
+        else:
+            self.org_brush = Image.merge("RGBA", (channels[2], channels[1], channels[0], channels[3]))
+            self.org_mask = channels[3]
         self.brush = self.org_brush.copy()
-        self.brush_color = color.lime
+
+        self.brush_color = color.white
+        self.brush_opacity = 1  # must be set!
         self.prev_pos = None
 
 
@@ -19,19 +29,39 @@ class Brush(Entity):
     @brush_color.setter
     def brush_color(self, value):
         self._brush_color = value
-        # self.brush_color = value
         self.value = (
-            int(value[0] * 255),
+            int(value[2] * 255),
             int(value[1] * 255),
-            int(value[2] * 255)
+            int(value[0] * 255)
             )
         tint = Image.new("RGBA", (self.brush.width, self.brush.height), self.value)
         self.brush = ImageChops.multiply(self.org_brush, tint)
+
+    @property
+    def brush_opacity(self):
+        return self._brush_opacity
+
+    @brush_opacity.setter
+    def brush_opacity(self, value):
+        # alpha = 1
+        try:
+            # self.mask = self.org_brush.convert("L").point(lambda x: min(x, value * 256))
+            # self.mask = channels[3]
+            self.mask = ImageEnhance.Brightness(self.org_mask).enhance(value)
+            print(self.mask)
+            # self.brush.putalpha(mask)
+        except:
+            print('fail')
 
 
     def input(self, key):
         if key == 'left mouse up':
             self.prev_pos = None
+
+        if key.isdigit():
+            if key == '0':
+                key = '10'
+            self.brush_opacity = int(key) / 10
 
 
     def update(self, dt):
@@ -50,14 +80,38 @@ class Brush(Entity):
                     pos_y = lerp(self.prev_pos[1], self.tex_y, i / steps)
                     base.canvas.img.paste(
                         self.brush,
-                        (int(pos_x - (self.brush.size[0] / 2)),
-                        int(pos_y - (self.brush.size[1] / 2))),
-                        self.brush)
+                        (int(pos_x - (self.brush.size[0] / 2)), int(pos_y - (self.brush.size[1] / 2))),
+                        self.mask)
             else:
+                # base.canvas.img = Image.alpha_composite(
+                # base.canvas.img,
+                # self.brush
+                # )
                 base.canvas.img.paste(
                     self.brush,
-                    (int(self.tex_x - (self.brush.size[0] / 2)),
-                    int(self.tex_y - (self.brush.size[1] / 2))),
-                    self.brush)
+                    (int(self.tex_x - (self.brush.size[0] / 2)), int(self.tex_y - (self.brush.size[1] / 2))),
+                    mask=self.mask)
 
             self.prev_pos = Vec3(self.tex_x, self.tex_y, 0)
+
+
+# if __name__ == '__main__':
+#     bg = Image.new('RGBA', (128, 128), (255, 255, 255, 0))
+#     brush = Image.open('textures/' + 'round_soft.png').transpose(Image.FLIP_TOP_BOTTOM)
+#
+#     value = color.green
+#     value = (
+#         int(value[2] * 255),
+#         int(value[1] * 255),
+#         int(value[0] * 255)
+#         )
+#     tint = Image.new("RGBA", (brush.width, brush.height), value)
+#     brush = ImageChops.multiply(brush, tint)
+#     # new = Image.alpha_composite(bg, brush)
+#     # new.show()
+#     bg.paste(
+#         brush,
+#         (int(0 - (brush.size[0] / 2)),
+#         int(0 - (brush.size[1] / 2))),
+#         brush)
+#     bg.show()
