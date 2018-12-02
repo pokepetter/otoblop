110b53b85914a5a4888f5fbd2a0aa7b2d9ba94eb
Petter Amland <pokepetter@gmail.com>
2018-12-02 01:28:17 +0100

    you can now draw when click outside the canvas. fixed some bugs after ursina update. brush graphic is now a circular line.

--- brush.py
+++ brush.py
@@ -1,31 +1,45 @@
 from ursina import *
 from PIL import Image, ImageChops, ImageEnhance
-
+from panda3d.core import Texture as PandaTexture
 
 
 class Brush(Entity):
     def __init__(self):
         super().__init__()
-        self.org_brush = Image.open('textures/' + 'round_soft.png').transpose(Image.FLIP_TOP_BOTTOM)
+        self.i = 0
+        self.org_brush = Image.open("textures/" + "round.png").transpose(
+            Image.FLIP_TOP_BOTTOM
+        )
         # convert to brg color
         channels = self.org_brush.split()
         printvar(len(channels))
         if len(channels) == 3:
-            self.org_brush = Image.merge("RGBA", (channels[2], channels[1], channels[0], 255))
+            self.org_brush = Image.merge(
+                "RGBA", (channels[2], channels[1], channels[0], 255)
+            )
         else:
-            self.org_brush = Image.merge("RGBA", (channels[2], channels[1], channels[0], channels[3]))
+            self.org_brush = Image.merge(
+                "RGBA", (channels[2], channels[1], channels[0], channels[3])
+            )
 
         self.brush = self.org_brush.copy()
         self.brush_color = color.black33
-        self.prev_pos = None
 
-        self.empty = Image.new("RGBA", (base.canvas.img.width, base.canvas.img.height), (255,0,0,128))
-        self.temp_layer_entity = Entity(parent = base.canvas, model='quad', z=-1, color=(1,1,1,.5))
-        self.temp_layer_buffer = Texture()
-        self.temp_layer_buffer.setup2dTexture(self.empty.width, self.empty.height, Texture.TUnsignedByte, Texture.FRgba)
-        self.temp_layer_buffer.setRamImageAs(self.empty.tobytes(), "RGBA")
-        self.temp_layer_entity.texture = self.temp_layer_buffer
-
+        self.temp_image = Image.new(
+            "RGBA", (base.canvas.img.width, base.canvas.img.height), (0, 0, 0, 0)
+        )
+        self.temp_layer = Entity(
+            parent=base.canvas, model="quad", z=-1, color=(1, 1, 1, .5)
+        )
+        self.temp_texture = Texture()
+        self.temp_texture._texture.setup2dTexture(
+            self.temp_image.width,
+            self.temp_image.height,
+            PandaTexture.TUnsignedByte,
+            PandaTexture.FRgba,
+        )
+        self.temp_texture._texture.setRamImageAs(self.temp_image.tobytes(), "RGBA")
+        self.temp_layer.texture = self.temp_texture
 
     @property
     def brush_color(self):
@@ -39,60 +53,91 @@ class Brush(Entity):
             int(value[1] * 255),
             int(value[0] * 255),
             # int(value[3] * 255)
-            )
+        )
         tint = Image.new("RGBA", (self.brush.width, self.brush.height), self.value)
         self.brush = ImageChops.multiply(self.org_brush, tint)
 
-
     def input(self, key):
-        if key == 'left mouse down':
-            self.empty = Image.new("RGBA", (base.canvas.img.width, base.canvas.img.height), (0,0,0,0))
-            self.temp_layer_entity.visible=True
+        if key == "left mouse down":
+            self.temp_image = Image.new(
+                "RGBA", (base.canvas.img.width, base.canvas.img.height), (0, 0, 0, 0)
+            )
+            self.temp_layer.visible = True
+            self.stamp(self.get_canvas_position())
 
-        if key == 'left mouse up':
-            self.prev_pos = None
+        if key == "left mouse up":
             # apply stroke
-            self.empty = Image.eval(self.empty, lambda x: x/2)
-            base.canvas.img = Image.alpha_composite(base.canvas.img, self.empty)
-            self.temp_layer_entity.visible=False
-            # self.empty = Image.new("RGBA", (base.canvas.img.width, base.canvas.img.height), (0,0,0,0))
+            self.temp_image = Image.eval(self.temp_image, lambda x: x / 2)
+            base.canvas.img = Image.alpha_composite(base.canvas.img, self.temp_image)
+            self.temp_layer.visible = False
+            self.temp_image = Image.new(
+                "RGBA", (base.canvas.img.width, base.canvas.img.height), (0, 0, 0, 0)
+            )
+
+        if key == "f":
+            print(mouse.hovered_entity)
+            # self.line_start = (int((mouse.point[0] + .5) * base.canvas.width), int((mouse.point[1] + .5) * base.canvas.height), 0)
+        if key == "f up":
+            print("draw line")
+            # self.draw_line(self.line_start, (int((mouse.point[0] + .5) * base.canvas.width), int((mouse.point[1] + .5) * base.canvas.height), 0))
 
         if key.isdigit():
-            if key == '0':
-                key = '10'
-            self.brush_opacity = int(key) / 10
+            if key == "0":
+                key = "10"
+            self.brush_color[3] = int(key) / 10
 
+    def get_canvas_position(self):
+        # returns pixel position
+        if mouse.hovered_entity == base.canvas.bg:
+            pos = (mouse.point * 10) + Vec3(.5, .5, 0)
+            pos = (pos[0] * base.canvas.width, pos[1] * base.canvas.height)
+            pos = (int(pos[0]), int(pos[1]), 0)
+            return pos
+
+    def stamp(self, position):
+        if not position:
+            return
+            
+        stamp_pos = (int(position[0] - (self.brush.size[0] / 2)), int(position[1] - (self.brush.size[1] / 2)))
+        try:
+            self.temp_image.alpha_composite(self.brush, stamp_pos)
+        except:
+            cropped_brush = self.brush.crop((
+                -stamp_pos[0], -stamp_pos[1],
+                self.brush.width, self.brush.height)
+            )
+            stamp_pos = (
+                max(int(position[0] - cropped_brush.width), 0) // 10,
+                max(int(position[1] - cropped_brush.height), 0) // 10
+                )
+            self.temp_image.alpha_composite(cropped_brush, stamp_pos)
+
+        self.last_drawn_point = position
+
+
+    def draw_line(self, start_pos, end_pos, spacing=64):
+        amount = int(distance(start_pos, end_pos) / spacing)
+        for i in range(1, amount):  # start at 1 to skip the first point
+            position = lerp(start_pos, end_pos, i / amount)
+            self.stamp(position)
+
+        # self.temp_texture.setRamImageAs(self.temp_image.tobytes(), "RGBA")
+        # self.temp_layer.texture = self.temp_texture
 
     def update(self):
-        if mouse.hovered_entity == base.canvas and mouse.left and mouse.point:
-            self.tex_x = int((mouse.point[0] + .5) * base.canvas.width)
-            self.tex_y = int((mouse.point[1] + .5) * base.canvas.height)
-            self.prev_point = (self.tex_x, self.tex_y)
+        # self.i += 1
+        if mouse.hovered_entity == base.canvas.bg and mouse.left and mouse.point:
+            self.draw_line(
+                self.last_drawn_point,
+                self.get_canvas_position(),
+                spacing=self.brush.width * .1,
+            )
+            # if self.i < 10:
+            #     return
 
-            # draw line between points
-            if self.prev_pos:
-                dist = distance(Vec3(self.prev_pos[0], self.prev_pos[1], 0), Vec3(self.tex_x, self.tex_y, 0))
-                # printvar(dist)
-                steps = int(dist / 5)
-                for i in range(steps):
-                    pos_x = lerp(self.prev_pos[0], self.tex_x, i / steps)
-                    pos_y = lerp(self.prev_pos[1], self.tex_y, i / steps)
-                    self.empty.alpha_composite(
-                        self.brush,
-                        (int(self.tex_x - (self.brush.size[0] / 2)), int(self.tex_y - (self.brush.size[1] / 2)))
-                    )
-            # else:
-            #     self.empty = Image.new("RGBA", (base.canvas.img.width, base.canvas.img.height), (0,0,0,0))
-            #     self.empty.alpha_composite(
-            #         self.brush,
-            #         (int(self.tex_x - (self.brush.size[0] / 2)), int(self.tex_y - (self.brush.size[1] / 2)))
-            #     )
-                # base.canvas.img.alpha_composite(self.empty)
-
-            self.prev_pos = Vec3(self.tex_x, self.tex_y, 0)
-
-            # update temp layer
-            # b = self.empty.tobytes()
-            self.temp_layer_buffer.setRamImageAs(self.empty.tobytes(), "RGBA")
-            # self.temp_layer_buffer.setRamImage(b)
-            self.temp_layer_entity.texture = self.temp_layer_buffer
+            if time.dt <= 1 / 60:
+                self.temp_texture._texture.setRamImageAs(
+                    self.temp_image.tobytes(), "RGBA"
+                )
+                self.temp_layer.texture = self.temp_texture
+                self.i = 0
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from ursina import *
 from PIL import Image, ImageChops
+
 # import opencv-python as ocv
 import numpy
 import time
 from brush import Brush
 from eyedropper import Eyedropper
 from palette import Palette
+from panda3d.core import Texture as PandaTexture
+
 
 def premultiply_alpha(img):
     # fake transparent image to blend with
     transparent = Image.new("RGBA", img.size, (0, 0, 0, 0))
     # blend with transparent image using own alpha
     return Image.composite(img, transparent, img)
+
 
 class Otoblop(Ursina):
     def __init__(self):
@@ -20,35 +24,30 @@ class Otoblop(Ursina):
         camera.orthographic = True
         camera.fov = 16
         window.color = color.gray
-        self.cursor = Cursor()
+        self.cursor = Cursor(model=Circle(mode="lines"))
 
-        self.canvas = Canvas(2048, 1024)
+        self.canvas = Canvas(512, 512)
 
         # instantiate tools
         self.brush = Brush()
         self.eyedropper = Eyedropper()
-        self.tools = (
-            self.brush,
-            self.eyedropper,
-            )
+        self.tools = (self.brush, self.eyedropper)
         self.active_tool = self.brush
 
         self.palette = Palette()
 
-
     def input(self, key):
         super().input(key)
-        if key == 'b':
+        if key == "b":
             self.active_tool = self.brush
 
-        if key == 'alt':
+        if key == "alt":
             self.prev_tool = self.active_tool
             self.active_tool = self.eyedropper
-            print('tyoo', self.active_tool)
+            print("tyoo", self.active_tool)
 
-        if key == 'alt up':
+        if key == "alt up":
             self.active_tool = self.prev_tool
-
 
     @property
     def active_tool(self):
@@ -56,7 +55,7 @@ class Otoblop(Ursina):
 
     @active_tool.setter
     def active_tool(self, value):
-        print('set active tool to:', value)
+        print("set active tool to:", value)
         for t in self.tools:
             if t is not value:
                 t.enabled = False
@@ -65,62 +64,61 @@ class Otoblop(Ursina):
         self._active_tool = value
 
 
-
 class Canvas(Entity):
     def __init__(self, width=1024, height=1024, color=color.white):
         super().__init__()
         self.scale *= 7
-        self.model = 'quad'
-        self.collider = 'box'
+        self.model = "quad"
+        # self.collider = "box"
+        # make a click area bigger than the canvas to be able to click outside and still draw.
+        self.bg = Entity(parent=self, z=1, collider="box", scale=(10, 10))
 
         self.i = 0
         # self.pressure = .1
         self.width = width
         self.height = height
-        self.scale_x *= width/height
+        self.scale_x *= width / height
 
-        self.img = Image.new('RGBA', (width, height), (255, 255, 255, 255))
+        self.img = Image.new("RGBA", (width, height), (255, 255, 255, 255))
 
         self.texture_buffer = Texture()
-        self.texture_buffer.setup2dTexture(width, height, Texture.TUnsignedByte, Texture.FRgba)
-        self.texture_buffer.setRamImageAs(self.img.tobytes(), "RGBA")
+        self.texture_buffer._texture.setup2dTexture(
+            width, height, PandaTexture.TUnsignedByte, PandaTexture.FRgba
+        )
+        self.texture_buffer._texture.setRamImageAs(self.img.tobytes(), "RGBA")
         self.texture = self.texture_buffer
-        self.texture.setRamImageAs(self.img.tobytes(), "RGBA")
-
+        self.texture._texture.setRamImageAs(self.img.tobytes(), "RGBA")
 
     def input(self, key):
-        if key == 'left mouse down':
+        if key == "left mouse down":
             self.undo_img = self.img.copy()
 
-        if key == 'scroll up':
+        if key == "scroll up":
             camera.fov -= 1
             camera.fov = max(camera.fov, 0)
 
-        if key == 'scroll down':
+        if key == "scroll down":
             camera.fov += 1
             camera.fov = min(camera.fov, 200)
 
-        if held_keys['control'] and key == 'z':
+        if held_keys["control"] and key == "z":
             # print('undo')
             if self.undo_img:
                 self.img = self.undo_img
                 b = self.img.tobytes()
-                self.texture_buffer.setRamImage(b)
+                self.texture_buffer._texture.setRamImage(b)
                 self.texture = self.texture_buffer
-
 
     def update(self):
 
-        if held_keys['space']:
+        if held_keys["space"]:
             camera.x -= mouse.velocity[0] * 10
             camera.y -= mouse.velocity[1] * 10
-
-
 
         self.i += 1
         if self.i > 2:  # update image less often to reduce lag.
             b = self.img.tobytes()
-            self.texture_buffer.setRamImage(b)
+            self.texture_buffer._texture.setRamImage(b)
             self.texture = self.texture_buffer
             self.i = 0
 
